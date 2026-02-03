85cbdd54244f49f4139cf47491bce0d9eaeee523
YouWin744 <YouWin744@outlook.com>
2026-02-03 15:50:32 +0800

    feat: basic screen

--- src/client/java/com/littletomato/warehouse/WarehouseKeyMapping.java
+++ src/client/java/com/littletomato/warehouse/WarehouseKeyMapping.java
@@ -4,9 +4,12 @@ import com.mojang.blaze3d.platform.InputConstants;
 import net.minecraft.client.KeyMapping;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
+import net.minecraft.client.Minecraft;
 import net.minecraft.network.chat.Component;
 import net.minecraft.resources.Identifier;
 import org.lwjgl.glfw.GLFW;
+
+import com.littletomato.warehouse.WarehouseScreen;
 
 public class WarehouseKeyMapping {
     public static void registerKeyBinding(String MOD_ID) {
@@ -26,6 +29,10 @@ public class WarehouseKeyMapping {
             while (openGUIKey.consumeClick()) {
                 if (client.player != null) {
                     client.player.displayClientMessage(Component.literal("Key Pressed!"), false);
+
+                    Minecraft.getInstance().setScreen(
+                            new WarehouseScreen(Component.empty())
+                    );
                 }
             }
         });
---
+++ src/client/java/com/littletomato/warehouse/WarehouseScreen.java
@@ -0,0 +1,42 @@
+package com.littletomato.warehouse;
+
+import net.minecraft.client.gui.GuiGraphics;
+import net.minecraft.client.gui.components.toasts.SystemToast;
+import net.minecraft.client.gui.screens.Screen;
+import net.minecraft.network.chat.Component;
+import net.minecraft.client.gui.components.Button;
+
+public class WarehouseScreen extends Screen {
+    public WarehouseScreen(Component title) {
+        super(title);
+    }
+
+    @Override
+    protected void init() {
+        Button buttonWidget = Button.builder(Component.literal("Hello World"), (btn) -> {
+            // When the button is clicked, we can display a toast to the screen.
+            this.minecraft.getToastManager().addToast(
+                    SystemToast.multiline(this.minecraft, SystemToast.SystemToastId.NARRATOR_TOGGLE,
+                            Component.nullToEmpty("Hello World!"), Component.nullToEmpty("This is a toast."))
+            );
+        }).bounds(40, 40, 120, 20).build();
+        // x, y, width, height
+        // It's recommended to use the fixed height of 20 to prevent rendering issues with the button
+        // textures.
+
+        // Register the button widget.
+        this.addRenderableWidget(buttonWidget);
+
+    }
+
+    @Override
+    public void render(GuiGraphics graphics, int mouseX, int mouseY, float delta) {
+        super.render(graphics, mouseX, mouseY, delta);
+
+        // Minecraft doesn't have a "label" widget, so we'll have to draw our own text.
+        // We'll subtract the font height from the Y position to make the text appear above the button.
+        // Subtracting an extra 10 pixels will give the text some padding.
+        // textRenderer, text, x, y, color, hasShadow
+        graphics.drawString(this.font, "Special Button", 40, 40 - this.font.lineHeight - 10, 0xFFFFFFFF, true);
+    }
+}
